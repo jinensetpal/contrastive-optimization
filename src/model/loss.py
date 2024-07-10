d7db8f901582b28a2967fc1f5dd341ec99146db6
Jinen Setpal <jinens8@gmail.com>
2024-07-11 00:59:07 +0530

    moved D_KL input to lgo space, fixed probability distribution on contrastive cam

--- src/model/loss.py
+++ src/model/loss.py
@@ -11,8 +11,9 @@ class ContrastiveLoss(nn.Module):
 
     def forward(self, y_pred, y):
         cc = self.get_contrastive_cam(y[1], y_pred[1])
+        cc -= cc.min()
         cc /= cc.sum()
         y[0] /= y[0].sum()
 
         # if const.QUANTILE_CLIP_CAMS: cc[(cc < cc.view(-1, 56**2).quantile(.1, dim=1) & (cc > cc.quantile(.1))] = 0
-        return self.kld(cc, y[0].repeat((cc.shape[1], 1, 1, 1)).permute(1, 0, 2, 3))
+        return self.kld(cc.log(), y[0].repeat((cc.shape[1], 1, 1, 1)).permute(1, 0, 2, 3))
