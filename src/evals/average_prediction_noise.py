46ba9a49f93d8e85f05f8e03cae8eb3026d45cd0
Jinen Setpal <jinens8@gmail.com>
2024-09-18 01:58:26 -0400

    added influence plots, prediction noise evals, removed dvc pipeline

---
+++ src/evals/average_prediction_noise.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+from ..data.oxford_iiit_pet import Dataset
+from torch.utils.data import DataLoader
+from ..model.arch import Model
+from src import const
+import random
+import torch
+import sys
+
+
+def average_prediction_noise(model, gen, misclassified_only=False):
+    avgs = torch.tensor([0., 0., 0.], device=const.DEVICE)
+    n_miscls = torch.tensor([0.], device=const.DEVICE)
+
+    for X, y in gen:
+        y_pred = model(X)
+        cc = model.get_contrastive_cams(y[1], y_pred[1]).detach().abs()
+        heatmap = y[0].repeat((cc.shape[1], 1, 1, 1)).permute(1, 0, 2, 3)
+
+        if misclassified_only:
+            mask = y[1].argmax(1) != y_pred[0].argmax(1)
+            cc = cc[mask]
+            heatmap = heatmap[mask]
+            n_miscls += mask.sum()
+
+        avgs[0] += cc.sum()
+        avgs[1] += cc[heatmap == 1].sum()
+        avgs[2] += cc[heatmap != 1].sum()
+
+    avgs /= n_miscls if misclassified_only else len(gen.dataset)
+    print('\n'.join([f'{feature} Average Influence: {metric}' for (feature, metric) in zip(('Net', 'Foreground', 'Background'), avgs.tolist())]))
+
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    random.seed(const.SEED)
+
+    model = Model(input_shape=const.IMAGE_SHAPE, is_contrastive='default' not in name)
+    model.load_state_dict(torch.load(const.MODELS_DIR / f'{name}.pt', map_location=const.DEVICE))
+    model.name = name
+    model.eval()
+
+    average_prediction_noise(model, DataLoader(Dataset('valid'), batch_size=10, shuffle=False), misclassified_only=len(sys.argv) == 3)
---
+++ src/evals/influence_comparison.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+
+from ..data.oxford_iiit_pet import Dataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from ..model.arch import Model
+from src import const
+import torchvision
+import random
+import torch
+import sys
+
+
+def evaluate_influence(model, gen, misclassified_only=False):
+    const.CAM_SIZE = const.IMAGE_SIZE
+    for batch_idx, (X, y) in enumerate(gen):
+        y_pred = model(X)
+        cc = model.get_contrastive_cams(y[1], y_pred[1]).detach().abs()
+        heatmap = y[0].repeat((cc.shape[1], 1, 1, 1)).permute(1, 0, 2, 3)
+
+        if misclassified_only:
+            mask = y[1].argmax(1) != y_pred[0].argmax(1)
+            cc = cc[mask]
+            heatmap = heatmap[mask]
+
+        for idx, (X_i, cc_i, heatmap_i) in enumerate(zip(X, cc, heatmap)):
+            fig = plt.figure()
+
+            fig.add_subplot(2, 2, 1)
+            plt.imshow(X_i.cpu().permute(1, 2, 0))
+            fig.add_subplot(2, 2, 2)
+            plt.imshow((X_i * heatmap_i).cpu().permute(1, 2, 0))
+
+            heatmap_i = torchvision.transforms.functional.resize(heatmap_i, (14, 14), antialias=False).squeeze(0)
+            fig.add_subplot(2, 2, 3)
+            plt.imshow((cc_i * heatmap_i)[0].cpu())
+            fig.add_subplot(2, 2, 4)
+            plt.imshow((cc_i * (1 - heatmap_i))[0].cpu())
+
+            fig.suptitle(f'Net Influence: {cc_i.sum()}\nForeground Influence: {cc_i[0][heatmap_i == 1].sum()}\nBackground Influence: {cc_i[0][heatmap_i != 1].sum()}')
+            plt.tight_layout()
+            fig.savefig(const.DATA_DIR / 'evals' / 'influence_plots' / f'{gen.dataset.split}_{batch_idx * gen.batch_size + idx}_{model.name}.png')
+            plt.close()
+
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    random.seed(const.SEED)
+
+    model = Model(input_shape=const.IMAGE_SHAPE, is_contrastive='default' not in name)
+    model.load_state_dict(torch.load(const.MODELS_DIR / f'{name}.pt', map_location=const.DEVICE))
+    model.name = name
+    model.eval()
+
+    evaluate_influence(model, DataLoader(Dataset('train'), batch_size=10, shuffle=False), misclassified_only=len(sys.argv) == 3)
